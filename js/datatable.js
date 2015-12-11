2efbe95f4186c9c37c6c810f7617c063663761b8
MaryJo Foster <MJ@MaryJoFoster.com>
2015-12-10 16:48:21 -0800

    - Initial Commit - Initialized connection to web server - Created initial' dataTable' {} for swapping server i/o - Created vote-tracker-table, on server, ready to receive data

---
+++ js/datatable.js
@@ -0,0 +1,56 @@
+// Main table for SQL calls
+
+var dataTable = [
+  ["clown", "Pennywise", "img/clown-1.jpg"],
+  ["clown", "Ronald Mcdonald", "img/clown-2.jpg"],
+  ["clown", "Circus Clowns", "img/clown-3.jpg"],
+  ["clown", "Twisty", "img/clown-4.jpg"],
+
+  ["dog", "Irish WolfHound", "img/dog-1.jpg"],
+  ["dog", "Crested Hairless", "img/dog-2.jpg"],
+  ["dog", "Golden Retriever", "img/dog-3.jpg"],
+
+  ["germ", "Common Cold", "img/germ-1.jpg"],
+  ["germ", "Germs", "img/germ-2.jpg"],
+  ["germ", "Germs", "img/germ-3.jpg"],
+
+  ["heights", "heights", "img/heights-1.jpg"],
+  ["heights", "heights", "img/heights-2.jpg"],
+  ["heights", "heights", "img/heights-3.jpg"],
+
+  ["kitten", "Kittens", "img/kitten-1.jpg"],
+  ["kitten", "Kittens", "img/kitten-2.jpg"],
+
+  ["needle", "Needle", "img/needles-1.jpg"],
+  ["needle", "Needle", "img/needles-2.jpg"],
+  ["needle", "Needle", "img/needles-3.jpg"],
+
+  ["snake", "Snake", "img/snake-1.jpg"],
+  ["snake", "Snake", "img/snake-2.jpg"],
+
+  ["spider", "Spider", "img/spider-1.jpg"],
+  ["spider", "Spider", "img/spider-2.jpg"],
+  ["spider", "Spider", "img/spider-3.jpg"],
+];
+
+// CREATE SQL table
+// function createTable() {
+//   CREATE TABLE vote-tracker-table (
+//     imgCategory TEXT,
+//     imgCaption TEXT,
+//     imgUrl TEXT
+//   )
+// };
+
+// INSERT record into SQL table
+function insertRecord() {
+
+};
+// SELECT record from SQL table
+function selectRecord() {
+
+};
+// UPDATE individual record from SQL table
+function updateRecord() {
+
+};
---
+++ js/webdb.js
@@ -0,0 +1,61 @@
+var webDB = {};
+webDB.sqlResult = null;
+
+$(function() {
+  webDB.init();
+});
+
+webDB.verbose = function (verbose) {
+  var msg;
+  if (verbose) {
+    html5sql.logInfo = true;
+    html5sql.logErrors = true;
+    html5sql.putSelectResultsInArray = true;
+    msg = 'html5sql verbosity on';
+  } else {
+    html5sql.logInfo = false;
+    html5sql.logErrors = false;
+    html5sql.putSelectResultsInArray = false;
+    msg = 'html5sql verbosity off';
+  }
+  console.log(msg);
+};
+
+webDB.init = function() {
+  // Open and init DB
+  try {
+    if (openDatabase) {
+      webDB.verbose(true);
+      webDB.connect('blogDB', 'Blog Database', 5*1024*1024);
+      webDB.setupTables();
+    } else {
+      console.log('Web Databases not supported.');
+    }
+  } catch (e) {
+    console.error(e);
+    console.error('Error occured during DB init. Web Database may not be supported.');
+  }
+};
+
+webDB.connect = function (database, title, size) {
+  html5sql.openDatabase(database, title, size);
+};
+
+webDB.setupTables = function () {
+  html5sql.process(
+    'CREATE TABLE IF NOT EXISTS articles (id INTEGER, title TEXT, category TEXT, author TEXT, authorUrl TEXT, publishedOn DATE, markdown TEXT)',
+    function() {
+      // on success
+    }
+  );
+};
+
+webDB.execute = function (sql, callback) {
+  callback = callback || function() {};
+  html5sql.process(
+    sql,
+    function (tx, result, resultArray) {
+      callback(resultArray);
+    }
+  );
+};
