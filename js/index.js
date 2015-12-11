c07ddf646151d1207500d6169a2cba481e9bc1a0
David Swaintek <dswaintek@gmail.com>
2015-12-11 15:03:55 -0800

    Created fillTable function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
-// Main JS code
-
+// // Main JS code
 $(function() {
-  webDB.init();  // Initialize connection to web server
+  webDB.init();
+  fillTable("photo_table", "category", "caption", "img_path", "s", "d", "f");
+  fillTable("category_table", "category", "score", "color", "s", "d", "f");
+  // webDB.execute('INSERT INTO photo_table (category, caption, img_path) VALUES ("d","e","f")') ;
 });
+
+
+
+
+
+
+
+var fillTable = function(tableName, column1, column2, column3, value1, value2, value3) {
+  webDB.execute('INSERT INTO '+tableName+' ('+column1+','+column2+','+column3+') VALUES ("'+value1+'","'+value2+'","'+value3+'");')
+};
+
+// $(function () {
+//   webDB.execute('INSERT INTO photo_table (category, caption, img_path) VALUES (a,b,c)', console.log("success") ) })
+
+// $(function () {
+//   for (var i = 0; i < dataTable.length; i++) {
+//   fillTable('photo_table', 'category', 'caption', 'img_path', '1', '2', '3')
+//   }
+// })
