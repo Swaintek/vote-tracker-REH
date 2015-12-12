6fe61b83c92d39f14c42672df4ceda1d1f766864
MaryJo Foster <MJ@MaryJoFoster.com>
2015-12-11 18:16:44 -0800

    - Complete for loops to build out data tables from dataTable (data array). - Updated dataTable (data array) to include category 'color'.

--- js/index.js
+++ js/index.js
@@ -1,26 +1,17 @@
 // // Main JS code
 $(function() {
   webDB.init();
-  fillTable("photo_table", "category", "caption", "img_path", "s", "d", "f");
-  fillTable("category_table", "category", "score", "color", "s", "d", "f");
+  for (var i = 0; i < dataTable.length; i++) { // [i][0] is the index to the category string
+    for (var j = 2; j < dataTable[i].length-2; j++) {
+      fillTable("photo_table", "category", "caption", "img_path", dataTable[i][0],dataTable[i][j][0], dataTable[i][j][1]);
+    };
+    fillTable("category_table", "category", "score", "color", dataTable[i][0],0,dataTable[i][1]);
+  };
+  // fillTable("photo_table", "category", "caption", "img_path", "s", "d", "f");
+  // fillTable("category_table", "category", "score", "color", "s", "d", "f");
   // webDB.execute('INSERT INTO photo_table (category, caption, img_path) VALUES ("d","e","f")') ;
 });
-
-
-
-
-
-
 
 var fillTable = function(tableName, column1, column2, column3, value1, value2, value3) {
   webDB.execute('INSERT INTO '+tableName+' ('+column1+','+column2+','+column3+') VALUES ("'+value1+'","'+value2+'","'+value3+'");')
 };
-
-// $(function () {
-//   webDB.execute('INSERT INTO photo_table (category, caption, img_path) VALUES (a,b,c)', console.log("success") ) })
-
-// $(function () {
-//   for (var i = 0; i < dataTable.length; i++) {
-//   fillTable('photo_table', 'category', 'caption', 'img_path', '1', '2', '3')
-//   }
-// })
